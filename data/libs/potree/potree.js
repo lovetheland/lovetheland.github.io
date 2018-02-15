34dc2f915bf3aa2c27eb5d43168c964c0b900344
lovetheland <32654329+lovetheland@users.noreply.github.com>
2018-02-15 02:27:41 -0500

    Update potree.js

--- data/libs/potree/potree.js
+++ data/libs/potree/potree.js
@@ -14235,8 +14235,8 @@ Potree.Scene = class extends THREE.EventDispatcher{
 		let light = new THREE.AmbientLight( 0x555555 ); // soft white light
 		this.scenePointCloud.add( light );
 		
-		let grid = Potree.utils.createGrid(5, 5, 2);
-		this.scene.add(grid);
+		//let grid = Potree.utils.createGrid(5, 5, 2);
+		//this.scene.add(grid);
 		
 		{// background
 		// var texture = THREE.ImageUtils.loadTexture( Potree.resourcePath + '/textures/background.gif' );
